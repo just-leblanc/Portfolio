c8cb94adb8e82e168d682516a98b8d815f88c0c1
Audrey BUFFET <162800294+just-leblanc@users.noreply.github.com>
2024-05-31 12:02:04 +0200

    interest

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sections = document.querySelectorAll('section');
     const menuLinks = document.querySelectorAll('.vertical-menu a');
+    const textSection = document.querySelector('.text'); 
 
     function activateMenuLink() {
-        let currentSection = sections[0];
-        
+        const textHeight = textSection.offsetHeight; 
+        const textScrollOffset = textSection.scrollTop; 
+        const visibleTop = textScrollOffset;
+        const visibleBottom = textScrollOffset + textHeight;
+    
         sections.forEach(section => {
-            const sectionTop = section.offsetTop - 150;
-            const sectionHeight = section.offsetHeight;
-            if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
-                currentSection = section;
-            }
-        });
-
-        menuLinks.forEach((link) => {
-            link.classList.remove('active');
-            if (link.getAttribute('href').substring(1) === currentSection.id) {
-                link.classList.add('active');
+            const sectionTop = section.offsetTop - textScrollOffset + 500; 
+            const sectionBottom = sectionTop + section.offsetHeight;
+    
+            const isVisible = (sectionBottom > visibleTop && sectionTop < visibleBottom);
+            if (isVisible) {
+                const sectionId = section.getAttribute('id');
+                menuLinks.forEach(link => {
+                    const linkId = link.getAttribute('href').substring(1);
+                    if (linkId === sectionId) {
+                        link.classList.add('active');
+                    } else {
+                        link.classList.remove('active');
+                    }
+                });
             }
         });
     }
-
     activateMenuLink();
-    window.addEventListener('scroll', activateMenuLink);
+    textSection.addEventListener('scroll', activateMenuLink);
 });
-
