39090fcb07d635977b0529703f0b665cb7940384
Audrey BUFFET <162800294+just-leblanc@users.noreply.github.com>
2024-05-31 10:39:55 +0200

    modif

--- scriptind.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#aboutButton').click(function() {
-        $('body').addClass('animate_content');
-        setTimeout(function() {
-            window.location.href = 'aboutme.html';
-        }, 1200); 
-    });
-});
